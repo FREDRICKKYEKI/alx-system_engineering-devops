138ededf22c79aaeecc43e9e7e7204bc45cf138c
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-25 20:55:37 +0300

    [fix]: task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -13,5 +13,6 @@ file { '/tmp/school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,5 +7,5 @@
 
 package { 'puppet-lint':
   ensure   => '2.1.0',
-  provider => 'gem'
+  provider => 'gem',
 }
